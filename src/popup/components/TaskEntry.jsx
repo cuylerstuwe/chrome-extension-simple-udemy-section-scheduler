982ef80d858235fbd7008cb81790e3258ea091a9
Cuyler Stuwe <cuylerkstuwe@gmail.com>
2022-05-27 20:53:12 -0700

    when user presses "Enter" within the text field, treat that as an intent to submit a todo-list item

--- src/popup/components/TaskEntry.jsx
+++ src/popup/components/TaskEntry.jsx
@@ -3,9 +3,14 @@ import React, {useState} from "react";
 const DEFAULT_TASK_ENTRY_TEXT = "";
 
 export default function TaskEntry({createAndAppendNewTaskWithDefaultSettings}) {
-
     const [taskEntryText, setTaskEntryText] = useState(DEFAULT_TASK_ENTRY_TEXT);
     const didUserActuallyEnterSomethingYet = !!(taskEntryText);
+
+    const triggerSubmitIntent = () => {
+        if(!didUserActuallyEnterSomethingYet) { return; }
+        createAndAppendNewTaskWithDefaultSettings(taskEntryText);
+        setTaskEntryText(DEFAULT_TASK_ENTRY_TEXT);
+    };
 
     return (
         <div style={{marginBottom: "10px"}}>
@@ -13,16 +18,18 @@ export default function TaskEntry({createAndAppendNewTaskWithDefaultSettings}) {
                 placeholder="New task title"
                 value={taskEntryText}
                 onChange={e => setTaskEntryText(e.target.value)}
+                onKeyDown={e => {
+                    if(e.key === "Enter") {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        triggerSubmitIntent();
+                    }
+                }}
             />
             <button
                 disabled={!didUserActuallyEnterSomethingYet}
                 style={{marginLeft: "5px"}}
-                onClick={() => {
-                    if(!didUserActuallyEnterSomethingYet) { return; }
-                    createAndAppendNewTaskWithDefaultSettings(taskEntryText);
-                    setTaskEntryText(DEFAULT_TASK_ENTRY_TEXT);
-                }
-            }>Add</button>
+                onClick={triggerSubmitIntent}>Add</button>
         </div>
     );
 }
